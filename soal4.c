10aee8ead27c92ba2dc3e381ed9f79af0a0e800f
irhammfadhil <irham.mfadhil@gmail.com>
2017-10-25 12:58:57 +0700

    minor fix

--- soal4.c
+++ soal4.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 typedef long long ll;
+
 pthread_t thread[100];
 
 void* fact (void* num) {
@@ -17,25 +18,18 @@ void* fact (void* num) {
         }
     }
     printf("Hasil %d! = %lld\n", a,res);
+    return NULL;
 }
 
 int main(int argc, char* argv[]) {
-	int i;j=1;
+	int i;int j=2;
 	int err;
-	int  num[argc-1];
-    for (i=1;i<argc;i++) num[i]=atoi[i];
+	int  num[argc];
+    for (i=2;i<argc;i++) num[i]=atoi(argv[i]);
     while(j<argc)//looping membuat thread 2x
     {
-        err=pthread_create(&(tid[j]),NULL,fact,(void*)num[j]);//membuat thread
-        if(err!=0)//cek error
-        {
-            printf("\n can't create thread : [%s]",strerror(err));
-        }
-        else
-        {
-            printf("\n create thread success");
-        }
-        pthread_join(tid[j],NULL);
+        err=pthread_create(&(thread[j]),NULL,fact,(void*)num[j]);//membuat thread
+        pthread_join(thread[j],NULL);
         j++;
     }
 	
