1541549e528f4f37ed15257253bacb5f6f6a3e20
irhammfadhil <irham.mfadhil@gmail.com>
2017-10-25 15:59:24 +0700

    prototipe word count

--- soal5.c
+++ soal5.c
@@ -6,6 +6,32 @@
 typedef long long ll;
 
 pthread_t thread[100];
+int wc(char* file_path, char* word){
+    FILE *fp;
+    int count = 0;
+    int ch, len;
+
+    if(NULL==(fp=fopen(file_path, "r")))
+        return -1;
+    len = strlen(word);
+    for(;;){
+        int i;
+        if(EOF==(ch=fgetc(fp))) break;
+        if((char)ch != *word) continue;
+        for(i=1;i<len;++i){
+            if(EOF==(ch = fgetc(fp))) goto end;
+            if((char)ch != word[i]){
+                fseek(fp, 1-i, SEEK_CUR);
+                goto next;
+            }
+        }
+        ++count;
+        next: ;
+    }
+end:
+    fclose(fp);
+    return count;
+}
 
 void* find (void* arg) {
 }
