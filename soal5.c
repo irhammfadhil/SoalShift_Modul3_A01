b6f565e2dc6c4db1304c719e336d1ff9fd73ab9d
irhammfadhil <irham.mfadhil@gmail.com>
2017-10-25 16:50:44 +0700

    minor fix

--- soal5.c
+++ soal5.c
@@ -1,49 +1,39 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
 #include <semaphore.h>
 #include <stdlib.h>
-
-typedef long long ll;
-
 pthread_t thread[100];
-int wc(char* file_path, char* word){
+void* freq (void *word) {
     FILE *fp;
-    int count = 0;
-    int ch, len;
-
-    if(NULL==(fp=fopen(file_path, "r")))
-        return -1;
-    len = strlen(word);
-    for(;;){
+    int count=0,ch,len;
+    char *w=(char*) word;
+    if(NULL==(fp=fopen("Novel.txt", "r"))) printf("-1\n");
+    len=strlen(word);
+    for (;;) {
         int i;
         if(EOF==(ch=fgetc(fp))) break;
-        if((char)ch != *word) continue;
-        for(i=1;i<len;++i){
-            if(EOF==(ch = fgetc(fp))) goto end;
-            if((char)ch != word[i]){
-                fseek(fp, 1-i, SEEK_CUR);
-                goto next;
-            }
-        }
-        ++count;
-        next: ;
+        if((char)ch != *w) continue;
+    	for(i=1;i<len;++i){
+    	if(EOF==(ch = fgetc(fp))) goto end;
+    	if((char)ch != w[i]){
+        	fseek(fp, 1-i, SEEK_CUR);
+        	goto next;
+    	}
+    }
+    ++count;
+    next: ;
     }
 end:
     fclose(fp);
-    return count;
+    printf("%s : %d\n",w,count);
+    return NULL;
 }
-
-void* find (void* arg) {
-}
-
 int main(int argc, char* argv[]) {
-    int j=1;
-    while(j<argc)//looping membuat thread 2x
-    {
-        err=pthread_create(&(thread[j]),NULL,fact,);//membuat thread
+    int j=1,err;
+    while(j<argc) {
+        err=pthread_create(&(thread[j]),NULL,freq,(void*)argv[j]);//membuat thread
         pthread_join(thread[j],NULL);
-        j++;
+        j++;        
     }
-	
-	
 }
