88b5329f235b8da5aa4829868ec23f70f56d6d4c
MRA1718 <33035393+MRA1718@users.noreply.github.com>
2017-10-25 17:59:13 +0700

    Melengkapi semua fungsi
    program gagal

--- soal3.c
+++ soal3.c
@@ -7,52 +7,85 @@ int lohan = 100;
 int kepiting = 100;
 int dec, sum;
 int choose;
-int status;
+//int status = 0;
 
 pthread_t tid1;
 pthread_t tid2;
 pthread_t tid3;
 
 void* menu(void *arg);
-void* dec_lohan(int *dec);
-void* dec_kepiting(int *sum);
+void* dec_lohan(void *arg);
+void* dec_kepiting(void *arg);
 
 int main()
 {
     printf("Simulasi Kolam Aidil\n");
     printf("Terdapat Lohan dan Kepiting\n\n");
 
-    p
+    pthread_create(&(tid1), NULL, &menu, (void*)&choose);
+    pthread_create(&(tid2), NULL, &dec_lohan, NULL);
+    pthread_create(&(tid3), NULL, &dec_kepiting, NULL);
+    
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+    
+    return 0;
 }
 
 void* menu(void *arg)
 {
+    int *choose=arg;
     while(lohan<=0 || kepiting <=0 || lohan>100 || kepiting>100)
     {
         printf("Apa yang mau kamu lakukan ?\n");
         printf("1. Tampilkan status\n");
         printf("2. Beri makan lohan\n");
         printf("3. Beri makan Kepiting\n");
-        printf("pilih salah satu : "); scanf("%d", &choose);
+        printf("pilih salah satu : "); scanf("%d", choose);
 
-        if(choose == 1)
+        if(*choose == 1)
         {
         printf("Status saat ini :\n");
         printf("Lohan : %d\n", lohan);
-        printf("kepiting : %d\n" kepiting);
+        printf("kepiting : %d\n", kepiting);
         } else
-            if (choose == 2)
+            if (*choose == 2)
             {
                 lohan = lohan + 10;
             } else
-                if (choose == 3)
+                if (*choose == 3)
                 {
                     kepiting = kepiting + 10;
                 } else 
                     {
                         printf ("Input salah\n");
-                        menu();
                     }
-        status = 1;
+        //status = 1;
     }
+}
+
+void* dec_lohan(void *arg)
+{
+    while(lohan<=0 || kepiting <=0 || lohan>100 || kepiting>100)
+    {
+        //if(status==1);
+        
+        lohan = lohan - 15;
+        
+        sleep(10);
+    }    
+}
+
+
+void* dec_kepiting(void *arg)
+{
+    while(lohan<=0 || kepiting <=0 || lohan>100 || kepiting>100)
+    {
+        //if(status==1);
+        
+        kepiting = kepiting - 10;
+        
+        sleep(20);
+    }    
 }
